e3dc2038cda5f36ffe08d6cf9ef8dc4c3943679f
Kerolos163 <kokoessa951@gmail.com>
2023-09-22 18:56:25 +0300

    Last Edit

--- lib/Feature/MapScreen/presentation/view/widget/buildFloatingSearchBar.dart
+++ lib/Feature/MapScreen/presentation/view/widget/buildFloatingSearchBar.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_maps/Core/Utils/mycolor.dart';
 import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
 class FloatingSearchBarWidget extends StatelessWidget {
   const FloatingSearchBarWidget({super.key, this.controller});
-    final  FloatingSearchBarController? controller;
+  final FloatingSearchBarController? controller;
   @override
   Widget build(BuildContext context) {
     final isPortrait =
         MediaQuery.of(context).orientation == Orientation.portrait;
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.only(top: 2.0),
       child: FloatingSearchBar(
-        controller: controller,
+          controller: controller,
+          elevation: 5,
           hint: 'Search...',
+          hintStyle: const TextStyle(fontSize: 18),
+          queryStyle: const TextStyle(fontSize: 18),
+          border: BorderSide.none,
+          iconColor: MyColors.blue,
           scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
           transitionDuration: const Duration(milliseconds: 800),
           transitionCurve: Curves.easeInOut,
@@ -24,14 +30,12 @@ class FloatingSearchBarWidget extends StatelessWidget {
           onQueryChanged: (query) {
             // Call your model, bloc, controller here.
           },
-          // Specify a custom transition to be used for
-          // animating between opened and closed stated.
           transition: CircularFloatingSearchBarTransition(),
           actions: [
             FloatingSearchBarAction(
               showIfOpened: false,
               child: CircularButton(
-                icon: const Icon(Icons.place),
+                icon: Icon(Icons.place, color: Colors.black.withOpacity(0.6)),
                 onPressed: () {},
               ),
             ),
